7dbd41ab23cc48d4de89cdf9fe233788bb199006
kojicollege <abe@setouchicollege.com>
2024-11-22 00:22:44 +0900

    テストの修正

--- app/Http/Requests/UpdatePasswordRequest.php
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -11,7 +11,7 @@ class UpdatePasswordRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdatePasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', Password::defaults(), 'confirmed'],
         ];
     }
 }
